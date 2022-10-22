ba0d424a0e577540700a0debcb3f14089fcd8423
Nikoloz Imerlishvili <formyauto75@gmail.com>
2022-10-23 01:19:54 +0400

    make it better

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 const { BOT_TOKEN } = require("../env.json");
+require('dotenv')
 
 module.exports = {
   Prefix: "$",
@@ -8,7 +9,7 @@ module.exports = {
   },
 
   Client: {
-    TOKEN: BOT_TOKEN,
+    TOKEN: BOT_TOKEN || process.env.RAILWAY_BOT_TOKEN,
     ID: "938136480453365770",
   },
 };
