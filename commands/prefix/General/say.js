75e57ad152b3f361bf96404670b9b3c6357659be
Nikoloz Imerlishvili <formyauto75@gmail.com>
2022-12-13 01:17:33 +0400

    fix( command no longer pings high order roles)

--- commands/prefix/General/say.js
+++ commands/prefix/General/say.js
@@ -1,5 +1,12 @@
 const { randomNumber } = require("./utils/randomNumber.utils");
 
+const randomRejectMesage = [
+  "you little slut, you think I will say that? <a:byegerlie:1051966912222998548>",
+  "yeah yeah keep writing that shit I won't say that",
+  "what about write command **$say <mention> is pussyless, huggless, bitchless and Loser** <a:K5iscoming:1051968101333356695>",
+  "ok nerd",
+];
+
 module.exports = {
   config: {
     name: "say",
@@ -9,14 +16,41 @@ module.exports = {
   permissions: ["SendMessages"],
   owner: false,
   run: async (_, message, args) => {
-
-    if(randomNumber()){
+    if (randomNumber()) {
       return message.reply({
         content: "stfu i won't say that",
       });
     }
 
+    const cmdAuthor = message.author.id;
+
+    const randomMessageIndex = Math.floor(
+      Math.random() * randomRejectMesage.length
+    );
+
     const argsF = message.content.split(" ").slice(1).join(" ");
+    const randomMessage = randomRejectMesage[randomMessageIndex];
+
+    if (
+      argsF.includes("@everyone") ||
+      argsF.includes("@here") ||
+      argsF.includes("<@&")
+    ) {
+      if (randomMessage.includes("<mention>")) {
+        const replaceWithMention = randomMessage.replace(
+          "<mention>",
+          `<@${cmdAuthor}>`
+        );
+        return message.reply({
+          content: replaceWithMention,
+        });
+      }
+
+      return message.reply({
+        content: randomMessage,
+      });
+    }
+
     if (!args[0]) {
       return message.reply({
         content: "Bitch WTF should I say???",
