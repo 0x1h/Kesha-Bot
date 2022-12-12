21a384929f194f8ff9ffc2e83867f50aedd51f82
Nikoloz Imerlishvili <formyauto75@gmail.com>
2022-12-13 02:12:47 +0400

    fix(karma-grammars)

--- commands/prefix/General/utils/karmaReplaceString.js
+++ commands/prefix/General/utils/karmaReplaceString.js
@@ -3,21 +3,30 @@
  */
 
 const karmaAnswers = [
-  '<replace> vincaa etyobaa ;DDDD',
-  '<replace> tavi gabia <:nawhbruhfrnowaybruh:1051976498686476348>',
-  'trakshi aravis gaercho, <replace> shen xar <:lolsha:1051976660418826261>',
-  'she <replace>o martla ;DDD wadi andzrie she bitchlesso',
-]
+  "<replace> vincaa etyobaa ;DDDD",
+  "<replace> tavi gabia <:nawhbruhfrnowaybruh:1051976498686476348>",
+  "trakshi aravis gaercho, <replace> shen xar <:lolsha:1051976660418826261>",
+  "she <replace>o martla ;DDD wadi andzrie she bitchlesso",
+];
 
 const karmaReplaceString = (message) => {
-  const findIndexOfSlur = message.split(' ').indexOf('xar')
-  const slurWord = message.split(' ')[findIndexOfSlur - 1]
+  const findIndexOfSlur = message.split(" ").indexOf("xar");
+  const randomKarmaIndex = Math.floor(Math.random() * karmaAnswers.length);
+  let slurWord = message.split(" ")[findIndexOfSlur - 1];
 
-  const randomKarmaIndex = Math.floor(Math.random() * karmaAnswers.length)
+  if (
+    slurWord.at(-1) === "i" &&
+    karmaAnswers[randomKarmaIndex].includes("<replace>o")
+  ) {
+    slurWord = slurWord.slice(0, -1)
+  }
 
-  const replaceWithSlur = karmaAnswers[randomKarmaIndex].replace('<replace>', slurWord)
+  const replaceWithSlur = karmaAnswers[randomKarmaIndex].replace(
+    "<replace>",
+    slurWord
+  );
 
-  return String(replaceWithSlur)
+  return String(replaceWithSlur);
 };
 
 module.exports = { karmaReplaceString };
