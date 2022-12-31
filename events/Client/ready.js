90816e97b84746051ec6c7b1081de7e0add81aa9
Nikoloz Imerlishvili <formyauto75@gmail.com>
2022-12-31 04:40:59 +0400

    feature(event-celebration)

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -1,10 +1,35 @@
 const client = require("../../index");
-const colors = require("colors");
+const { eventSender } = require("./eventSender");
+require("colors");
+ 
+const CronJob = require("cron").CronJob;
+
+const cronExpression = "59 23 31 11 *";
+
+client.on("ready", () => {
+  function newYearAnniversary() {
+    console.log("Sent...".bgGreen);
+
+    eventSender(client)
+  }
+
+  const cronJob = new CronJob(
+    cronExpression,
+    newYearAnniversary,
+    null,
+    true,
+    "Asia/Tbilisi"
+  );
+
+  cronJob.start();
+});
 
 module.exports = {
-  name: "ready.js"
+  name: "ready.js",
 };
 
-client.once('ready', async () => {
-  console.log("\n" + `[READY] ${client.user.tag} is up and ready to go.`. bgGreen);
-})
+client.once("ready", async () => {
+  console.log(
+    "\n" + `[READY] ${client.user.tag} is up and ready to go.`.bgGreen
+  );
+});
