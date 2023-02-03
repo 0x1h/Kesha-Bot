4781b083ecb17ffe5c46c72fe28f3d834fdb5bc6
callmenikk <formyauto75@gmail.com>
2023-02-03 19:56:15 +0400

    fix(undefined-files)

--- commands/slash/confess/utils/fileUpload.js
+++ commands/slash/confess/utils/fileUpload.js
@@ -1,20 +1,27 @@
-const fileUpload = ({attachment, mediaUrl}) => {
-  const file = attachment || mediaUrl
+const fileUpload = ({ attachment, mediaUrl }) => {
+  const file = attachment || mediaUrl;
 
-  const extension = file.split('.').at(-1) 
-  const isVideo = ['mov', 'mp4', 'webm'].some(e => e === extension)
-
-  if(!isVideo && file !== undefined){
+  if (!file) {
     return {
       video: undefined,
-      photo: file
-    }
+      photo: undefined,
+    };
+  }
+
+  const extension = file.split(".").at(-1);
+  const isVideo = ["mov", "mp4", "webm"].some((e) => e === extension);
+
+  if (!isVideo && file !== undefined) {
+    return {
+      video: undefined,
+      photo: file,
+    };
   }
 
   return {
     video: file,
-    photo: undefined
-  }
-}
+    photo: undefined,
+  };
+};
 
-module.exports = {fileUpload}
+module.exports = { fileUpload };
